b519b8b962b4d6c8bd4fe74e6628c5d1c8810c02
annanrsujay <118834408+annanrsujay@users.noreply.github.com>
2022-11-27 12:42:11 +0530

    Add files via upload

---
+++ sketch_nov15c.ino
@@ -0,0 +1,25 @@
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(5,4);
+Adafruit_Fingerprint finger= Adafruit_Fingerprint(&mySerial);
+uint8_t id;
+void setup()
+{
+  Serial.begin(9600);
+  while(!Serial);
+  delay(100);
+  Serial.println("\n\nAdafruit Fingerprint Sensor enrollment");
+  finger.begin(57600);
+  if(finger.verifyPassword())
+  {
+    Serial.println("Found FingerPrint Sensor");
+  }
+  else
+  {
+    Serial.println("Did not find fingerprint sensor");
+    while (1)
+    {
+      delay(1);
+    }
+  }
+}
